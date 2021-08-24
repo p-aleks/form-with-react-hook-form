d7bcb3b5e0edd0f24a0a4baaa8fd2a9a8f43c3b8
A. P <alekspilipets@gmail.com>
2021-08-24 12:46:38 -0700

    Updates

--- src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import { ReactComponent as MaleIcon } from './maleIcon.svg';
-import { ReactComponent as FemaleIcon } from './femaleIcon.svg';
-import { ReactComponent as OtherIcon } from './otherIcon.svg';
-import { ReactComponent as EyeIcon } from './eyeIcon.svg';
-
-
-export { MaleIcon, FemaleIcon, OtherIcon, EyeIcon };

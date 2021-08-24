d1e84fcb7817bbfd393e005be64c032a3d89c1a9
A. P <alekspilipets@gmail.com>
2021-08-24 12:00:07 -0700

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as MaleIcon } from './maleIcon.svg';
+import { ReactComponent as FemaleIcon } from './femaleIcon.svg';
+import { ReactComponent as OtherIcon } from './otherIcon.svg';
+import { ReactComponent as EyeIcon } from "./eyeIcon.svg";
+
+
+export {MaleIcon, FemaleIcon, OtherIcon, EyeIcon};
---
+++ src/components/UI/label/MyRadioLabelCard/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+const MyRadioLabelCard = ({ text, Icon }) => {
+    return (
+        <div className="card">
+            <Icon />
+            <span>
+                {text}
+            </span>
+        </div>
+    );
+};
+
+export default MyRadioLabelCard;
